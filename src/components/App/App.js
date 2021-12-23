4ee71d4937dd658c30b626d71600577e78b9d75b
Joshua Benefield <benefield.joshua@gmail.com>
2021-12-23 03:05:38 -0700

    Initial Commit

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from '../Header/Header'
+import Container from '@mui/material/Container'
+
+
+
+function App() {
+  return (
+    <Container maxWidth='xs'>
+      <div className="App">
+        <Header />
+      </div>
+    </Container>
+  );
+}
+
+export default App;
