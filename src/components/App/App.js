a580cc9c5820fa5c86f2302ea426a21a8dfc203c
Joshua Benefield <benefield.joshua@gmail.com>
2021-12-24 00:00:08 -0700

    Make drawer swipeable

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,8 +5,18 @@ import Header from '../Header/Header'
 import HomePage from '../HomePage/HomePage'
 import LogIn from '../LogIn/LogIn'
 import Container from '@mui/material/Container'
-import Drawer from '@mui/material/Drawer'
-import Button from '@mui/material/Button'
+import SwipeableDrawer from '@mui/material/SwipeableDrawer'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+
+import IconButton from '@mui/material/IconButton'
+
+import HomeIcon from '@mui/icons-material/Home'
+import CollectionsIcon from '@mui/icons-material/Collections'
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
 
 
 function App() {
@@ -28,9 +38,43 @@ function App() {
           />
         </Routes>
       </div>
-      <Drawer anchor='left' open={modal}>
-        <Button onClick={() => toggleModal(false)} variant='outlined' sx={{width: '200px'}}>test</Button>
-      </Drawer>
+      <SwipeableDrawer anchor='left' open={modal} sx={{display: 'flex', flexDirection: 'column'}}>
+        <List>
+
+          <ListItem disablePadding sx={{width: '300px'}}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary='Home' />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding sx={{width: '300px'}}>
+            <ListItemButton>
+              <ListItemIcon>
+                <CollectionsIcon />
+              </ListItemIcon>
+              <ListItemText primary='Gallery' />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding sx={{width: '300px'}}>
+            <ListItemButton>
+              <ListItemIcon>
+                <AttachMoneyIcon />
+              </ListItemIcon>
+              <ListItemText primary='Pricing' />
+            </ListItemButton>
+          </ListItem>
+
+        </List>
+        <Container sx={{alignSelf: 'flex-end'}}>
+          <IconButton>
+            <HomeIcon />
+          </IconButton>
+        </Container>
+      </SwipeableDrawer>
     </Container>
   );
 }
