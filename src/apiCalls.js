7e92f802fa190719f617308280833d305833baa2
Joshua Benefield <benefield.joshua@gmail.com>
2021-12-24 04:31:18 -0700

    Allow users to create accounts

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,18 @@
 const apiCalls = {
   logIn(user) {
-    console.log('fetch')
     return fetch('https://nails-by-rylee-server.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(response => response.json())
+      .catch(error => console.log(error))
+  },
+
+  register(user) {
+    return fetch('https://nails-by-rylee-server.herokuapp.com/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,27 +1,35 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import Alert from '@mui/material/Alert'
 import apiCalls from '../../apiCalls'
+import './LogIn.css'
 
 const LogIn = ({ updateUser }) => {
   const [email, changeEmail] = React.useState('')
   const [password, changePassword] = React.useState('')
+  const [firstName, changeFirstName] = React.useState('')
+  const [lastName, changeLastName] = React.useState('')
+  const [phone, changePhone] = React.useState('')
   const [register, toggleForm] = React.useState(false)
   const [error, updateError] = React.useState(false)
   const [errorMessage, updateErrorMessage] = React.useState('')
 
   const logIn = async () => {
-    register && toggleForm(false)
+    if(register) {
+      toggleForm(false)
+      handleError(false, '')
+      return
+    }
     const user = await apiCalls.logIn({ email, password })
     user[0] ? saveUserInfo(user[0]) : handleError(true, `We couldn't find an account with those credentials`)
-
   }
 
   const saveUserInfo = user => {
+    console.log(user)
     updateUser(user)
     localStorage.setItem('user', JSON.stringify(user))
   }
@@ -31,29 +39,41 @@ const LogIn = ({ updateUser }) => {
     updateErrorMessage(message)
   }
 
+  const registerUser = async () => {
+    if(!register) {
+      toggleForm(true)
+      handleError(false, '')
+      return
+    }
+    const newUser = await apiCalls.register({firstName, lastName, email, password})
+    newUser[0] ? saveUserInfo(newUser[0]) : handleError(true, 'That email already exists')
+  }
 
-  const registerUser = e => {
-    !register && toggleForm(true)
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('hi')
   }
 
   return (
-    <Grid container mt={4} sx={{backgroundColor: 'rgba(255, 255, 255, 0.5)', minWidth: '350px', maxWidth: '750px'}} >
-      <Grid item xs={12} mt={4}>
-        <Typography variant='h3'>Log In</Typography>
-      </Grid>
-      <Grid item xs={12} mt={4} >
-        <TextField id='email' error={error} label='Email' type='email' variant='outlined' onChange={e => changeEmail(e.target.value)} sx={{minWidth: '300px'}} />
-      </Grid>
-      <Grid item xs={12} mt={4} >
-        <TextField id='password' error={error} label='Password' type='password' variant='outlined' onChange={e => changePassword(e.target.value)} sx={{minWidth: '300px'}} />
-      </Grid>
-      <Grid item xs={12} my={4} >
-        {error && <Alert severity='error'>{errorMessage}</Alert>}
-        <Button onClick={logIn} variant='outlined' sx={{ mx: 1}}>Log In</Button>
-        <Button onClick={registerUser} variant='outlined' sx={{ mx: 1}}>Register</Button>
-      </Grid>
-      {register && <><p>hi</p></>}
-    </Grid>
+    <Paper sx={{mt: '20px', }}>
+      <form className='log-in-form' onSubmit={handleSubmit}>
+        <Typography variant='h3' my={2}>Log In</Typography>
+        {register &&
+          <>
+        <TextField id='first-name' error={error} label='First Name' variant='outlined' onChange={e => changeFirstName(e.target.value)} sx={{width: '50%', my: 2}} />
+        <TextField id='last-name' error={error} label='Last Name' variant='outlined' onChange={e => changeLastName(e.target.value)} sx={{width: '50%', my: 2}} />
+        <TextField id='phone' error={error} label='Phone Number' type='phone' variant='outlined' onChange={e => changePhone(e.target.value)} sx={{width: '50%', my: 2}} />
+          </>
+        }
+          <TextField  id='email' error={error} label='Email' type='email' variant='outlined' onChange={e => changeEmail(e.target.value)} sx={{width: '50%', my: 2}} />
+          <TextField  id='password' error={error} label='Password' type='password' variant='outlined' onChange={e => changePassword(e.target.value)} sx={{width: '50%', my: 2}} />
+        <div>
+          {error && <Alert severity='error'>{errorMessage}</Alert>}
+          <Button onClick={logIn} variant='outlined' sx={{ mx: 1, my: 2}}>Log In</Button>
+          <Button onClick={registerUser} variant='outlined' sx={{ mx: 1, my: 2}}>Register</Button>
+        </div>
+      </form>
+    </Paper>
   )
 }
 
