c94a41e01c6769ee837d5dbe5deaa74ef708b1aa
Joshua Benefield <benefield.joshua@gmail.com>
2021-12-27 05:25:48 -0700

    Update log in

--- src/components/HomePage/Services.js
+++ src/components/HomePage/Services.js
@@ -13,10 +13,10 @@ const Services = () => {
       </Grid>
 
       <Grid item xs={2} m={2}>
-        <Card sx={{maxWidth: '500px', minWidth: {xs: '300px'}}}>
+        <Card sx={{maxWidth: '500px', minWidth: {xs: '200px'}}}>
           <CardHeader avatar={<DateRangeIcon />} title='Service 1' titleTypographyProps={{fontSize: '25px'}} sx={{backgroundColor: '#E06C9F'}} />
           <CardContent sx={{backgroundColor: '#F9C8DF'}}>
-            <Typography variant='body2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</Typography>
+            <Typography variant='body2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </Typography>
           </CardContent>
         </Card>
       </Grid>
